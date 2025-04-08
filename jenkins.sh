694d868e52ea7a60bd1064ef6754c87eeb07bf90
AdidelaHarishReddy <adidelaharish201@gmail.com>
2025-04-08 22:39:23 +0530

    Create jenkins.sh
    its the jenkins installing code for linux

---
+++ jenkins.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "1.updating...."
+sudo apt update
+echo "instaling open jdk-21 for run java support jenkins..."
+sudo apt install -y openjdk-21-jdk
+echo "adding jenkins package to apt list ...."
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+  echo "aded jenkins to apt and installing.... "
+sudo apt-get update
+sudo apt-get install jenkins
